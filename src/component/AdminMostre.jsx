410a51adb69b676f20effee0ca907b2f82523c0b
MicheleFamoso <michelefamoso@gmail.com>
2025-09-10 19:25:20 +0200

    Add placeholders to AdminMostre component
    Added placeholder paragraphs for DATA, LUOGO, TITOLO, and DESCRIZIONE to the AdminMostre component to outline future content structure.

--- src/component/AdminMostre.jsx
+++ src/component/AdminMostre.jsx
@@ -1,5 +1,17 @@
 const AdminMostre = () => {
-  return <h1>Mostre</h1>;
+  return (
+    <div>
+       <h1>Mostre</h1>
+      <p>DATA</p>
+      <p>LUOGO</p>
+      <p>TITOLO</p>
+      <p>DESCRIZIONE</p>
+    </div>
+     
+
+
+
+  )
 };
 
 export default AdminMostre;
