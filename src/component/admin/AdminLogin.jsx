009677bc244f31fce5e229cf97f8489872ca215b
MicheleFamoso <michelefamoso@gmail.com>
2025-10-11 10:13:00 +0200

    Add show/hide password toggle to admin login
    Introduced a show/hide password feature using EyeIcon and EyeSlashIcon in the admin login form. Improved form styling and fixed error message color and font.

--- src/component/admin/AdminLogin.jsx
+++ src/component/admin/AdminLogin.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/outline";
+
 const AdminLogin = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const navigate = useNavigate();
+  const [showPassword,setShowPassword]=useState(false)
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -42,9 +45,9 @@ const AdminLogin = () => {
     <div className="flex justify-center h-dvh  bg-panna ">
       <form
         onSubmit={handleSubmit}
-        className=" h-fit px-8 py-6 mt-50   "
+        className=" h-fit my-auto   "
       >
-        <h1 className=" text-center mb-10 text-2xl font-bold font-kosugi text-gray-extraBold">Login</h1>
+        <h1 className=" text-center mb-6 text-2xl font-bold font-kosugi text-gray-extraBold">Login</h1>
         <div className="flex flex-col">
           <label htmlFor="email" className=" ml-4 mb-2 font-kosugi text-gray-bold">
             Email
@@ -54,28 +57,38 @@ const AdminLogin = () => {
             type="email"
             value={email}
             onChange={(e)=> setEmail(e.target.value)}
-            className="border inset-shadow-sm font-kosugi text-gray-bold pl-4 border-panna-medium/80 bg-panna-medium p-1 rounded-2xl focus:outline-hidden focus:ring-2 focus:ring-gray-mediumBold"
+            className="border inset-shadow-sm font-kosugi  text-gray-bold pl-4 border-panna-medium/80 bg-panna-medium p-1 rounded-2xl focus:outline-hidden focus:ring-2 focus:ring-gray-mediumBold"
           />
         </div>
         <div className="flex flex-col mt-6">
-          <label htmlFor="password" className=" ml-4 mb-2">
+          <label htmlFor="password" className=" ml-4 mb-2 font-kosugi text-gray-bold">
             Password
           </label>
-          <input
+          <div className="relative">
+             <input
             id="password"
-            type="password"
+            type={showPassword ? "text":"password"}
             value={password}
             onChange={(e)=> setPassword(e.target.value)}
             className="border inset-shadow-sm font-kosugi text-gray-bold pl-4 border-panna-medium/80 bg-panna-medium p-1 rounded-2xl focus:outline-hidden focus:ring-2 focus:ring-gray-mediumBold"
           />
+          <button
+           type="button"
+          onClick={() => setShowPassword(!showPassword)}
+          className="absolute right-3 top-1 bg-panna-medium "
+          >
+            {showPassword ? <EyeIcon className="size-6 text-gray-bold"/> : <EyeSlashIcon className="size-6 text-gray-bold "/> }
+          </button>
+          </div>
+         
         </div>
         {Error && (
-            <p className="mt-2 text-center font-bold text-red-600 text-shadow-2xs">{error}</p>
+            <p className="mt-2 text-center font-bold text-red-400 font-kosugi text-shadow-2xs">{error}</p>
         )}
         <div className="mt-6 text-center">
           <button
             type="submit"
-            className="bg-gray-mediumBold/60 px-10 py-2 rounded-3xl inset-shadow-sm font-bold text-xl text-gray-extraBold hover:bg-gray-mediumBold hover:text-white font-kosugi"
+            className="cursor-pointer bg-gray-mediumBold/60 px-10 py-2 rounded-3xl inset-shadow-sm font-bold text-xl text-gray-extraBold hover:bg-gray-mediumBold hover:text-white font-kosugi"
           >
             Accedi
           </button>
