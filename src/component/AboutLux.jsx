2e23aab6eb77b030b5d21988de49fffc8c92435d
MicheleFamoso <michelefamoso@gmail.com>
2025-11-11 17:49:45 +0100

    fix

--- src/component/AboutLux.jsx
+++ src/component/AboutLux.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
+import Loading from "./Loading";
 
 const AboutLux = () => {
   const [bios, setBios] = useState([]);
-
+  const [isLoading, setIsloading] = useState(true);
   const handleBio = async () => {
     try {
       const response = await fetch(
@@ -14,6 +15,7 @@ const AboutLux = () => {
       }
       const data = await response.json();
       setBios(data);
+      setIsloading(false);
     } catch (error) {
       console.error("errore", error);
     }
@@ -25,65 +27,71 @@ const AboutLux = () => {
   return (
     <div className=" mt-2 md:mt-6 md:w-8/12 md:m-auto static px-3 md:px-0">
       <div>
-        {bios.map((bio) => (
-          <div key={bio.id}>
-            <div className="px-4 md:px-0">
-              <h2 className="mt-8 md:text-6xl text-4xl font-kosugi">
-                LUCIANO FAMOSO
-              </h2>
-              <h3 className="font-kosugi font-extrabold  text-2xl  text-gray-bold">
-                VISUAL ARTIST
-              </h3>
-            </div>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <div>
+            {bios.map((bio) => (
+              <div key={bio.id}>
+                <div className="px-4 md:px-0">
+                  <h2 className="mt-8 md:text-6xl text-4xl font-kosugi">
+                    LUCIANO FAMOSO
+                  </h2>
+                  <h3 className="font-kosugi font-extrabold  text-2xl  text-gray-bold">
+                    VISUAL ARTIST
+                  </h3>
+                </div>
 
-            <div className="grid md:grid-cols-3 grid-cols-1 mt-6 ">
-              <div className="px-4 md:px-0 md:col-span-2">
-                {" "}
-                <p className="mt-2 font-light text-2xl md:text-3xl font-reenie md:mb-16 mb-6 text-gray-extraBold">
-                  {bio.bio}
-                </p>
-                <div className="md:mt-6">
-                  <a
-                    href={`mailto:${bio.email}`}
-                    className="font-kosugi md:text-3xl text-xl text-gray-bold font-bold"
-                  >
+                <div className="grid md:grid-cols-3 grid-cols-1 mt-6 ">
+                  <div className="px-4 md:px-0 md:col-span-2">
                     {" "}
-                    {bio.email}
-                  </a>
-                </div>
-                <div className="md:mt-3">
-                  <a
-                    href="https://is.gd/luxfamososhop"
-                    className="font-kosugi md:text-3xl text-xl text-gray-bold font-bold"
-                  >
-                    Lux Shop
-                  </a>
+                    <p className="mt-2 font-light text-2xl md:text-3xl font-reenie md:mb-16 mb-6 text-gray-extraBold">
+                      {bio.bio}
+                    </p>
+                    <div className="md:mt-6">
+                      <a
+                        href={`mailto:${bio.email}`}
+                        className="font-kosugi md:text-3xl text-xl text-gray-bold font-bold"
+                      >
+                        {" "}
+                        {bio.email}
+                      </a>
+                    </div>
+                    <div className="md:mt-3">
+                      <a
+                        href="https://is.gd/luxfamososhop"
+                        className="font-kosugi md:text-3xl text-xl text-gray-bold font-bold"
+                      >
+                        Lux Shop
+                      </a>
+                    </div>
+                  </div>
+                  <div className="md:ml-30 px-4 md:px-0 mt-10 md:mt-0 ">
+                    <h3 className="font-kosugi font-extrabold  md:text-4xl text-2xl  text-gray-extraBold">
+                      SERVICES
+                    </h3>
+                    <ul>
+                      <li className="font-kosugi font-bold md:text-2xl text-xl text-gray-bold mb-1">
+                        Opere su commissione
+                      </li>
+                      <li className="font-kosugi font-bold  md:text-2xl text-xl text-gray-bold mb-1">
+                        <a href="https://is.gd/luxfamososhop">
+                          Vendita opere e stampe
+                        </a>
+                      </li>
+                      <li className="font-kosugi font-bold  md:text-2xl text-xl text-gray-bold mb-1">
+                        Collaborazioni creative
+                      </li>
+                      <li className="font-kosugi font-bold  md:text-2xl text-xl text-gray-bold mb-1">
+                        Mostre e installazioni
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               </div>
-              <div className="md:ml-30 px-4 md:px-0 mt-10 md:mt-0 ">
-                <h3 className="font-kosugi font-extrabold  md:text-4xl text-2xl  text-gray-extraBold">
-                  SERVICES
-                </h3>
-                <ul>
-                  <li className="font-kosugi font-bold md:text-2xl text-xl text-gray-bold mb-1">
-                    Opere su commissione
-                  </li>
-                  <li className="font-kosugi font-bold  md:text-2xl text-xl text-gray-bold mb-1">
-                    <a href="https://is.gd/luxfamososhop">
-                      Vendita opere e stampe
-                    </a>
-                  </li>
-                  <li className="font-kosugi font-bold  md:text-2xl text-xl text-gray-bold mb-1">
-                    Collaborazioni creative
-                  </li>
-                  <li className="font-kosugi font-bold  md:text-2xl text-xl text-gray-bold mb-1">
-                    Mostre e installazioni
-                  </li>
-                </ul>
-              </div>
-            </div>
+            ))}
           </div>
-        ))}
+        )}
       </div>
     </div>
   );
--- src/component/AwarsLux.jsx
+++ src/component/AwarsLux.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
 import { ArrowUpCircleIcon } from "@heroicons/react/24/solid";
+import Loading from "./Loading";
 const AwardsLux = () => {
   const [mostre, setMostre] = useState([]);
-
+  const [isLoading, setIsloading] = useState(true);
   const handleMostre = async () => {
     try {
       const res = await fetch(
@@ -11,6 +12,7 @@ const AwardsLux = () => {
       if (!res.ok) throw new Error("Errore nel caricamento delle mostre");
       const data = await res.json();
       setMostre(data);
+      setIsloading(false);
       console.log(data);
     } catch (err) {
       console.log(err);
@@ -26,36 +28,44 @@ const AwardsLux = () => {
       <h1 className="text-6xl font-bold font-kosugi px-6 md:px-0   text-gray-medium mb-12 ">
         Awards/Show
       </h1>
-      {mostre.map((mostra, index) => (
-        <div key={mostra.id} className=" mb-16">
-          <div className="grid grid-cols-1 md:grid-cols-3 md:gap-6 gap-1 px-6 md:px-0">
-            <div className="font-kosugi  md:mb-0">
-              <h2 className="md:text-4xl text-3xl text-gray-extraBold font-bold">
-                {mostra.titolo}
-              </h2>
-              <p className="md:text-2xl text-xl font-semibold text-gray-mediumBold">
-                {mostra.luogo}
-              </p>
-            </div>{" "}
-            <div className="md:col-span-2 font-kosugi">
-              <p className="md:text-3xl text-2xl font-semibold  text-gray-medium ">
-                {mostra.descrizione}
-              </p>
-              <p className=" text-gray-mediumBold text-end mt-2">
-                {mostra.data}
-              </p>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div>
+          {mostre.map((mostra, index) => (
+            <div key={mostra.id} className=" mb-16">
+              <div className="grid grid-cols-1 md:grid-cols-3 md:gap-6 gap-1 px-6 md:px-0">
+                <div className="font-kosugi  md:mb-0">
+                  <h2 className="md:text-4xl text-3xl text-gray-extraBold font-bold">
+                    {mostra.titolo}
+                  </h2>
+                  <p className="md:text-2xl text-xl font-semibold text-gray-mediumBold">
+                    {mostra.luogo}
+                  </p>
+                </div>{" "}
+                <div className="md:col-span-2 font-kosugi">
+                  <p className="md:text-3xl text-2xl font-semibold  text-gray-medium ">
+                    {mostra.descrizione}
+                  </p>
+                  <p className=" text-gray-mediumBold text-end mt-2">
+                    {mostra.data}
+                  </p>
+                </div>
+              </div>
+              {index !== mostre.length - 1 && (
+                <hr className=" mx-2 md:mx-0 m-2 md:m-6 text-gray-light" />
+              )}
             </div>
+          ))}
+          <div className="flex justify-center ">
+            <button
+              onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+            >
+              <ArrowUpCircleIcon className="w-10 text-gray-bold hover:text-gray-extraBold  cursor-pointer" />
+            </button>
           </div>
-          {index !== mostre.length - 1 && (
-            <hr className=" mx-2 md:mx-0 m-2 md:m-6 text-gray-light" />
-          )}
         </div>
-      ))}
-      <div className="flex justify-center ">
-        <button onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}>
-          <ArrowUpCircleIcon className="w-10 text-gray-bold hover:text-gray-extraBold  cursor-pointer" />
-        </button>
-      </div>
+      )}
     </div>
   );
 };
