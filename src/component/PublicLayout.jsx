70d5316f5abc20345cfe05e015470ec157b3e7a3
MicheleFamoso <michelefamoso@gmail.com>
2025-11-11 16:01:42 +0100

    Update PublicLayout.jsx

--- src/component/PublicLayout.jsx
+++ src/component/PublicLayout.jsx
@@ -1,23 +1,22 @@
-
 import { Outlet } from "react-router-dom";
-import LuxNavBar from "./LuxNavBar"; 
+import LuxNavBar from "./LuxNavBar";
 import WorkLux from "./WorkLux";
 import FooterLux from "./FooterLux";
-const PublicLayout =()=>{
-    return(
-         <div className="  grid grid-rows-[auto_1fr_auto] min-h-screen bg-panna  ">
-            <header className=" ">
-                <LuxNavBar/>
-            </header>
-            <main className="">
-                <Outlet/>
-              
-            </main>
-            <footer className="">
-                <FooterLux/>
-            </footer>
-          </div>
-    )
-}
+const PublicLayout = () => {
+  return (
+    <div className="  grid grid-rows-[auto_1fr_auto] min-h-screen bg-panna  ">
+      <header className=" ">
+        <LuxNavBar />
+      </header>
+      <main className="">
+        <WorkLux />
+        <Outlet />
+      </main>
+      <footer className="">
+        <FooterLux />
+      </footer>
+    </div>
+  );
+};
 
-export default PublicLayout
+export default PublicLayout;
