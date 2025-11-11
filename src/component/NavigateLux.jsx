fd8d8ac44fdf37c57cfc8bc59c9047f7d150306f
MicheleFamoso <michelefamoso@gmail.com>
2025-11-11 16:14:53 +0100

    Update NavigateLux.jsx

--- src/component/NavigateLux.jsx
+++ src/component/NavigateLux.jsx
@@ -4,22 +4,22 @@ const Navigatelux = () => {
   const navigate = useNavigate();
   return (
     <div>
-      <div className="flex flex-col md:flex-row justify-center gap-15.5 mt-16">
+      <div className="flex flex-col md:flex-row justify-center gap-15.5 mt-16 mb-20">
         <button
           onClick={() => navigate("/work")}
-          className="font-kosugi text-4xl font-bold text-gray-light hover:text-gray-bold cursor-pointer md:transition md:delay-150 md:duration-300 md:ease-in-out md:hover:-translate-y-2 md:hover:scale-120"
+          className="font-kosugi text-4xl font-extrabold text-gray-light hover:text-gray-bold cursor-pointer md:transition md:delay-150 md:duration-300 md:ease-in-out md:hover:-translate-y-2 md:hover:scale-120"
         >
           Work
         </button>
         <button
           onClick={() => navigate("/awards")}
-          className="font-kosugi text-4xl font-bold text-gray-light hover:text-gray-bold cursor-pointer md:transition md:delay-150 md:duration-300 md:ease-in-out md:hover:-translate-y-2 md:hover:scale-120"
+          className="font-kosugi text-4xl font-extrabold text-gray-light hover:text-gray-bold cursor-pointer md:transition md:delay-150 md:duration-300 md:ease-in-out md:hover:-translate-y-2 md:hover:scale-120"
         >
           Awards/Shows
         </button>
         <button
           onClick={() => navigate("/about")}
-          className="font-kosugi text-4xl font-bold text-gray-light hover:text-gray-bold cursor-pointer md:transition md:delay-150 md:duration-300 md:ease-in-out md:hover:-translate-y-2 md:hover:scale-120"
+          className="font-kosugi text-4xl font-extrabold text-gray-light hover:text-gray-bold cursor-pointer md:transition md:delay-150 md:duration-300 md:ease-in-out md:hover:-translate-y-2 md:hover:scale-120"
         >
           About
         </button>
