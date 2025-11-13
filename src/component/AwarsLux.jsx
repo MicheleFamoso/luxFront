45f44669f7a07b5fccc54ee642791580e5e34014
MicheleFamoso <michelefamoso@gmail.com>
2025-11-13 18:15:45 +0100

    Update AwarsLux.jsx

--- src/component/AwarsLux.jsx
+++ src/component/AwarsLux.jsx
@@ -25,7 +25,7 @@ const AwardsLux = () => {
 
   return (
     <div className=" mt-2 md:mt-6 md:w-8/12 md:m-auto static">
-      <h1 className="text-6xl font-bold font-kosugi px-6 md:px-0   text-gray-medium mb-12 ">
+      <h1 className="text-5xl md:text-6xl font-bold font-kosugi px-6 md:px-0   text-gray-medium mb-12 ">
         Awards/Show
       </h1>
       {isLoading ? (
@@ -33,21 +33,21 @@ const AwardsLux = () => {
       ) : (
         <div>
           {mostre.map((mostra, index) => (
-            <div key={mostra.id} className=" mb-16">
-              <div className="grid grid-cols-1 md:grid-cols-3 md:gap-6 gap-1 px-6 md:px-0">
-                <div className="font-kosugi  md:mb-0">
-                  <h2 className="md:text-4xl text-3xl text-gray-extraBold font-bold">
+            <div key={mostra.id} className=" my-8">
+              <div className="grid grid-cols-1 md:grid-cols-3 md:gap-6 mb-8 gap-0 px-6 md:px-0">
+                <div className="font-kosugi md:mb-0  mb-4">
+                  <h2 className="md:text-4xl text-2xl text-gray-extraBold font-bold">
                     {mostra.titolo}
                   </h2>
-                  <p className="md:text-2xl text-xl font-semibold text-gray-mediumBold">
+                  <p className="md:text-2xl text-lg font-semibold text-gray-mediumBold">
                     {mostra.luogo}
                   </p>
                 </div>{" "}
                 <div className="md:col-span-2 font-kosugi">
-                  <p className="md:text-3xl text-2xl font-semibold  text-gray-medium ">
+                  <p className="md:text-3xl text-xl font-semibold  text-gray-medium ">
                     {mostra.descrizione}
                   </p>
-                  <p className=" text-gray-mediumBold text-end mt-2">
+                  <p className=" text-gray-mediumBold text-end md:mb-2">
                     {mostra.data}
                   </p>
                 </div>
