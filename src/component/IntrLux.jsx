2fbc679f732f98e8a20faff4cc7be396de53d3f0
MicheleFamoso <michelefamoso@gmail.com>
2026-04-11 09:47:30 +0200

    .

--- src/component/IntrLux.jsx
+++ src/component/IntrLux.jsx
@@ -4,14 +4,14 @@ const IntrLux = () => {
       <div className="bg-hero bg-cover bg-center  rounded-4xl shadow-lg transition-all duration-500 ease-in-out  md:h-80 h-70 w-80 md:w-150 pt-10">
         <div className="flex justify-center  md:items-center ">
           <img
-            className="object-cover rounded-full 
+            className="object-cover rounded-full
               md:w-30 w-20 opacity-80 "
-            src="/img-profile.jpg"
+            src="/Lx.webp"
             alt="img-profile"
           />
         </div>
         <div
-          className="text-center   font-kosugi 
+          className="text-center   font-kosugi
              md:text-4xl text-3xl mt-6"
         >
           <p className="text-gray-medium font-black">VISUAL ARTIST</p>
